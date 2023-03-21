ef140d8213a3a94d8609c30c53c808f97aaded17
Kishangaur24 <kishangaur24@gmail.com>
2023-03-21 12:48:45 +0530

    Gym project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {  BrowserRouter  } from 'react-router-dom';
+import Navbar from './Component/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Login from './Pages/Login';
+import Registration from './Pages/Registration';
+import WholeTeam from './Section/WholeTeam';
+import Activity from './Pages/Activity';
+import Activity1 from './Pages/Activity1';
+import { RecoilRoot } from 'recoil';
+import { Routes , Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <RecoilRoot>
+   <BrowserRouter>
+   <Navbar/>
+   
+   <div className="pages">
+   <Routes>
+    <Route exact path='/' element={<Home/>}></Route>
+    <Route exact path='/about' element={<About/>}></Route>
+    <Route exact path='/login' element={<Login/>}></Route>
+    <Route exact path='/registration' element={<Registration/>}></Route>
+    <Route exact path='/wholeteam' element={<WholeTeam/>}></Route>
+    <Route exact path='/activity' element={<Activity/>}></Route>
+    <Route exact path='/activity1' element={<Activity1/>}></Route>
+   </Routes>
+   </div>
+   </BrowserRouter>
+   </RecoilRoot>
+   
+   
+   
   );
 }
 
