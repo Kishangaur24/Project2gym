4f8b15f5b844b32013285e29c42b176e629b81fa
Kishangaur24 <kishangaur24@gmail.com>
2023-04-25 11:10:27 +0530

    update carousel

--- src/Pages/Program.js
+++ src/Pages/Program.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Carousel} from "react-bootstrap"
 import './Program.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Program() {
   return (
     <div id='program'>
